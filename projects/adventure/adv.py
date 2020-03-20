1401c1a81c83004a1b2713f00a920c5813847c93
vishnuyar <vardhanvishnu@gmail.com>
2020-03-21 03:44:30 +0530

    956 moves complete

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -14,7 +14,7 @@ world = World()
 #map_file = "maps/test_line.txt"
 #map_file = "maps/test_cross.txt"
 #map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
+#map_file = "maps/test_loop_fork.txt"
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
@@ -24,13 +24,14 @@ world.load_graph(room_graph)
 # Print an ASCII map
 world.print_rooms()
 
-player = Player(world.starting_room)
+
+
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
 
-def room_recursive(starting_room,room_graph,room_dict=None,visit_list=None):
+
+def room_recursive(starting_room,room_graph,room_dict=None,visit_list=None,previous_direction=None):
     if visit_list is None:
         visit_list = []
     if room_dict is None:
@@ -42,9 +43,11 @@ def room_recursive(starting_room,room_graph,room_dict=None,visit_list=None):
         directions = starting_room.get_exits()
         for dir in directions:
             room_dict[room_no].update({dir:starting_room.get_room_in_direction(dir).id})
-        for direction in starting_room.get_exits():
+        directions = starting_room.get_exits()
+        random.shuffle(directions)
+        for direction in directions:
             new_room = starting_room.get_room_in_direction(direction)
-            room_recursive(new_room,room_graph,room_dict,visit_list)
+            room_recursive(new_room,room_graph,room_dict,visit_list,direction)
     if len(room_dict) == len(room_graph):
         return room_dict,visit_list
     
@@ -77,39 +80,48 @@ def bfs(starting_vertex, destination_vertex,room_dict):
                     dirpath_copy.append(direction)
                     bft_queue.enqueue(path_copy)
                     dir_queue.enqueue(dirpath_copy)
+room_order = []
+min_steps = 2000
+for step in range(200000):
+    traversal_path = []
+    #world.starting_room = world.rooms[i]
+    player = Player(world.starting_room)
+    #Traverse all the rooms in the world using recursive dft 
+    room_dict,visited = room_recursive(world.starting_room,room_graph)
+    #print(visited)
+    #print(room_dict)
+    for i in range(len(visited)-1):
+        #Get the shortest between the two rooms
+        path = bfs(visited[i],visited[i+1],room_dict)
+        #print(f'{visited[i]} to {visited[i+1]} in {len(path)} ')
+        traversal_path.extend(path)
 
 
 
-#Traverse all the rooms in the world using recursive dft 
-room_dict,visited = room_recursive(world.starting_room,room_graph)
-#print(visited)
-#print(room_dict)
-for i in range(len(visited)-1):
-    #Get the shortest between the two rooms
-    path = bfs(visited[i],visited[i+1],room_dict)
-    #print(f'{visited[i]} to {visited[i+1]} in {len(path)} ')
-    traversal_path.extend(path)
-
-
-
-
-# TRAVERSAL TEST
-visited_rooms = set()
-player.current_room = world.starting_room
-visited_rooms.add(player.current_room)
-
-for move in traversal_path:
-    player.travel(move)
+    # TRAVERSAL TEST
+    visited_rooms = set()
+    player.current_room = world.starting_room
     visited_rooms.add(player.current_room)
 
-if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
-else:
-    print("TESTS FAILED: INCOMPLETE TRAVERSAL")
-    print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
+    for move in traversal_path:
+        player.travel(move)
+        visited_rooms.add(player.current_room)
 
-
-
+    if len(visited_rooms) == len(room_graph):
+        print(f"Times:{step} , TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    else:
+        print("TESTS FAILED: INCOMPLETE TRAVERSAL")
+        print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
+    if min_steps > len(traversal_path):
+        min_steps = len(traversal_path)
+        room_order = visited
+        optimum_path = traversal_path.copy()
+print('---------------')
+print(f'The minimum number of steps is {min_steps}') 
+print('---------------')
+#print(f'The order of rooms visited is {visited}')
+print('---------------')
+print(f'Traversal {optimum_path}')
 #######
 # UNCOMMENT TO WALK AROUND
 #######
